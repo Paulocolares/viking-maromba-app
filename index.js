f55bd075cd0a8f761287a930334a35b0725a1626
Paulocolares <paulocolares.ceo@gmail.com>
2025-06-14 00:17:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Viking Maromba 💪</h1>
+      <p>Seu app de treinos em máquinas está funcionando!</p>
+    </div>
+  );
+}
